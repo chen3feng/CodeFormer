3290ef14cfe1901a2541907de6841f61a234b027
chen3feng <chen3feng@gmail.com>
2023-02-01 20:30:26 +0800

    Some optimizations:
    * Support webp input
    * Reduce memory footprint when merge video
    * Add progress bar in video merging

--- inference_codeformer.py
+++ inference_codeformer.py
@@ -9,6 +9,7 @@ from basicsr.utils.download_util import load_file_from_url
 from basicsr.utils.misc import gpu_is_available, get_device
 from facelib.utils.face_restoration_helper import FaceRestoreHelper
 from facelib.utils.misc import is_gray
+from tqdm import tqdm
 
 from basicsr.utils.registry import ARCH_REGISTRY
 
@@ -16,13 +17,14 @@ pretrain_model_url = {
     'restoration': 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth',
 }
 
+
 def set_realesrgan():
     from basicsr.archs.rrdbnet_arch import RRDBNet
     from basicsr.utils.realesrgan_utils import RealESRGANer
 
     use_half = False
-    if torch.cuda.is_available(): # set False in CPU/MPS mode
-        no_half_gpu_list = ['1650', '1660'] # set False for GPUs that don't support f16
+    if torch.cuda.is_available():  # set False in CPU/MPS mode
+        no_half_gpu_list = ['1650', '1660']  # set False for GPUs that don't support f16
         if not True in [gpu in torch.cuda.get_device_name(0) for gpu in no_half_gpu_list]:
             use_half = True
 
@@ -47,37 +49,46 @@ def set_realesrgan():
     if not gpu_is_available():  # CPU
         import warnings
         warnings.warn('Running on CPU now! Make sure your PyTorch version matches your CUDA.'
-                        'The unoptimized RealESRGAN is slow on CPU. '
-                        'If you want to disable it, please remove `--bg_upsampler` and `--face_upsample` in command.',
-                        category=RuntimeWarning)
+                      'The unoptimized RealESRGAN is slow on CPU. '
+                      'If you want to disable it, please remove `--bg_upsampler` and `--face_upsample` in command.',
+                      category=RuntimeWarning)
     return upsampler
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-i', '--input_path', type=str, default='./inputs/whole_imgs',
-            help='Input image, video or folder. Default: inputs/whole_imgs')
+                        help='Input image, video or folder. Default: inputs/whole_imgs')
     parser.add_argument('-o', '--output_path', type=str, default=None,
-            help='Output folder. Default: results/<input_name>_<w>')
+                        help='Output folder. Default: results/<input_name>_<w>')
     parser.add_argument('-d', '--device', type=str, default=None,
                         help='Computing device, can be cpu|cuda|mps|dml')
     parser.add_argument('-w', '--fidelity_weight', type=float, default=0.5,
-            help='Balance the quality and fidelity. Default: 0.5')
+                        help='Balance the quality and fidelity. Default: 0.5')
     parser.add_argument('-s', '--upscale', type=int, default=2,
-            help='The final upsampling scale of the image. Default: 2')
-    parser.add_argument('--has_aligned', action='store_true', help='Input are cropped and aligned faces. Default: False')
-    parser.add_argument('--only_center_face', action='store_true', help='Only restore the center face. Default: False')
-    parser.add_argument('--draw_box', action='store_true', help='Draw the bounding box for the detected faces. Default: False')
+                        help='The final upsampling scale of the image. Default: 2')
+    parser.add_argument('--has_aligned', action='store_true',
+                        help='Input are cropped and aligned faces. Default: False')
+    parser.add_argument('--only_center_face', action='store_true',
+                        help='Only restore the center face. Default: False')
+    parser.add_argument('--draw_box', action='store_true',
+                        help='Draw the bounding box for the detected faces. Default: False')
     # large det_model: 'YOLOv5l', 'retinaface_resnet50'
     # small det_model: 'YOLOv5n', 'retinaface_mobile0.25'
     parser.add_argument('--detection_model', type=str, default='retinaface_resnet50',
-            help='Face detector. Optional: retinaface_resnet50, retinaface_mobile0.25, YOLOv5l, YOLOv5n. \
+                        help='Face detector. Optional: retinaface_resnet50, retinaface_mobile0.25, YOLOv5l, YOLOv5n. \
                 Default: retinaface_resnet50')
-    parser.add_argument('--bg_upsampler', type=str, default='None', help='Background upsampler. Optional: realesrgan')
-    parser.add_argument('--face_upsample', action='store_true', help='Face upsampler after enhancement. Default: False')
-    parser.add_argument('--bg_tile', type=int, default=400, help='Tile size for background sampler. Default: 400')
-    parser.add_argument('--suffix', type=str, default=None, help='Suffix of the restored faces. Default: None')
-    parser.add_argument('--save_video_fps', type=float, default=None, help='Frame rate for saving video. Default: None')
+    parser.add_argument('--bg_upsampler', type=str, default='None',
+                        help='Background upsampler. Optional: realesrgan')
+    parser.add_argument('--face_upsample', action='store_true',
+                        help='Face upsampler after enhancement. Default: False')
+    parser.add_argument('--bg_tile', type=int, default=400,
+                        help='Tile size for background sampler. Default: 400')
+    parser.add_argument('--suffix', type=str, default=None,
+                        help='Suffix of the restored faces. Default: None')
+    parser.add_argument('--save_video_fps', type=float, default=None,
+                        help='Frame rate for saving video. Default: None')
 
     args = parser.parse_args()
 
@@ -85,10 +96,10 @@ if __name__ == '__main__':
     # ------------------------ input & output ------------------------
     w = args.fidelity_weight
     input_video = False
-    if args.input_path.endswith(('jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG')): # input single img path
+    if args.input_path.lower().endswith(('jpg', 'jpeg', 'png', 'webp')):  # input single img path
         input_img_list = [args.input_path]
         result_root = f'results/test_img_{w}'
-    elif args.input_path.endswith(('mp4', 'mov', 'avi', 'MP4', 'MOV', 'AVI')): # input video path
+    elif args.input_path.lower().endswith(('mp4', 'mov', 'avi')):  # input video path
         from basicsr.utils.video_util import VideoReader, VideoWriter
         input_img_list = []
         vidreader = VideoReader(args.input_path)
@@ -102,14 +113,14 @@ if __name__ == '__main__':
         result_root = f'results/{video_name}_{w}'
         input_video = True
         vidreader.close()
-    else: # input img folder
+    else:  # input img folder
         if args.input_path.endswith('/'):  # solve when path ends with /
             args.input_path = args.input_path[:-1]
         # scan all the jpg and png images
         input_img_list = sorted(glob.glob(os.path.join(args.input_path, '*.[jpJP][pnPN]*[gG]')))
         result_root = f'results/{os.path.basename(args.input_path)}_{w}'
 
-    if not args.output_path is None: # set output path
+    if not args.output_path is None:  # set output path
         result_root = args.output_path
 
     test_img_num = len(input_img_list)
@@ -134,11 +145,11 @@ if __name__ == '__main__':
 
     # ------------------ set up CodeFormer restorer -------------------
     net = ARCH_REGISTRY.get('CodeFormer')(dim_embd=512, codebook_size=1024, n_head=8, n_layers=9,
-                                            connect_list=['32', '64', '128', '256']).to(device)
+                                          connect_list=['32', '64', '128', '256']).to(device)
 
     # ckpt_path = 'weights/CodeFormer/codeformer.pth'
     ckpt_path = load_file_from_url(url=pretrain_model_url['restoration'],
-                                    model_dir='weights/CodeFormer', progress=True, file_name=None)
+                                   model_dir='weights/CodeFormer', progress=True, file_name=None)
     checkpoint = torch.load(ckpt_path)['params_ema']
     net.load_state_dict(checkpoint)
     net.eval()
@@ -157,7 +168,7 @@ if __name__ == '__main__':
         args.upscale,
         face_size=512,
         crop_ratio=(1, 1),
-        det_model = args.detection_model,
+        det_model=args.detection_model,
         save_ext='png',
         use_parse=True,
         device=device)
@@ -172,7 +183,7 @@ if __name__ == '__main__':
             basename, ext = os.path.splitext(img_name)
             print(f'[{i+1}/{test_img_num}] Processing: {img_name}')
             img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        else: # for video processing
+        else:  # for video processing
             basename = str(i).zfill(6)
             img_name = f'{video_name}_{basename}' if input_video else basename
             print(f'[{i+1}/{test_img_num}] Processing: {img_name}')
@@ -225,15 +236,18 @@ if __name__ == '__main__':
             face_helper.get_inverse_affine(None)
             # paste each restored face to the input image
             if args.face_upsample and face_upsampler is not None:
-                restored_img = face_helper.paste_faces_to_input_image(upsample_img=bg_img, draw_box=args.draw_box, face_upsampler=face_upsampler)
+                restored_img = face_helper.paste_faces_to_input_image(
+                    upsample_img=bg_img, draw_box=args.draw_box, face_upsampler=face_upsampler)
             else:
-                restored_img = face_helper.paste_faces_to_input_image(upsample_img=bg_img, draw_box=args.draw_box)
+                restored_img = face_helper.paste_faces_to_input_image(
+                    upsample_img=bg_img, draw_box=args.draw_box)
 
         # save faces
         for idx, (cropped_face, restored_face) in enumerate(zip(face_helper.cropped_faces, face_helper.restored_faces)):
             # save cropped face
             if not args.has_aligned:
-                save_crop_path = os.path.join(result_root, 'cropped_faces', f'{basename}_{idx:02d}.png')
+                save_crop_path = os.path.join(
+                    result_root, 'cropped_faces', f'{basename}_{idx:02d}.png')
                 imwrite(cropped_face, save_crop_path)
             # save restored face
             if args.has_aligned:
@@ -258,17 +272,16 @@ if __name__ == '__main__':
         # load images
         video_frames = []
         img_list = sorted(glob.glob(os.path.join(result_root, 'final_results', '*.[jp][pn]g')))
-        for img_path in img_list:
-            img = cv2.imread(img_path)
-            video_frames.append(img)
+        img = cv2.imread(img_list[0])
         # write images to video
-        height, width = video_frames[0].shape[:2]
+        height, width = img.shape[:2]
         if args.suffix is not None:
             video_name = f'{video_name}_{args.suffix}.png'
         save_restore_path = os.path.join(result_root, f'{video_name}.mp4')
         vidwriter = VideoWriter(save_restore_path, height, width, fps, audio)
 
-        for f in video_frames:
+        for img_path in tqdm(img_list):
+            f = cv2.imread(img_path)
             vidwriter.write_frame(f)
         vidwriter.close()
 
